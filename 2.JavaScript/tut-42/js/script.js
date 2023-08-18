de8b2ae3bf46119edd41fd5aa846a6d2e3eb9db4
Lenovo <battleshipcomputer@gmail.com>
2023-08-18 09:47:08 +0530

    Initial commit

---
+++ 2.JavaScript/tut-42/js/script.js
@@ -0,0 +1,34 @@
+// do while and for loop
+
+/* 
+// while loop ...
+let count = 1;
+
+if (count === 0) {
+    console.log(`count should not be ${count}`);
+} else {
+    while (count <= 5) {
+        console.log(`total count is: ${count}`);
+        count++;
+    }
+}
+ */
+
+/* 
+// this is do while loop...
+let count = 0;
+
+    do {
+        console.log(`total count is: ${count}`);
+        count++;
+    } while (count <= 5);
+ */
+
+
+    // for loop over...
+let num = 5;
+let count = 0;
+for (let i = 0; i < num; i++) {
+    count++;
+    console.log(`total count is: ${count}`);
+}
