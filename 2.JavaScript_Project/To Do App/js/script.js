29b7fe6a2be1ac96a147ecc7262c5d0d34ddc31f
Lenovo <battleshipcomputer@gmail.com>
2023-08-26 00:52:29 +0530

    javascript project [to do app] updated...

---
+++ 2.JavaScript_Project/To Do App/js/script.js
@@ -0,0 +1,51 @@
+const inputBox = document.getElementById("input_box");
+const listContainer = document.getElementById("list_container");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("Please enter a task name");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener(
+    "click",
+    function (e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveData();
+        } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    },
+    false
+);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
+// Event listener for the Enter key
+input_box.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        addTask(); // Call the addTask function when Enter is pressed
+    }
+});
+
+// Event listener for the Add button
+// add_btn.addEventListener('click', addTask);
